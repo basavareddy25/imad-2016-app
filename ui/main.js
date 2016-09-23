b404bf427d9e2918e909b2dc80b7d36a1a9f40a7
basavareddy25 <basavareddy25@gmail.com>
2016-09-23 20:47:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML="i am the boss";
 "i am the boss";
+element.onclick=function()
+{
 element.style.marginleft='1000px';
+};
