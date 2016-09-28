109f6ba794e20f2e8977d8b68fc592a9ead138cd
basavareddy25 <basavareddy25@gmail.com>
2016-09-28 19:11:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+
 });
 app.get('/artical-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
