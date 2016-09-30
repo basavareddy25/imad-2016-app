f21ad66622589a6a67b9db82572f8f08c1a5b0f0
basavareddy25 <basavareddy25@gmail.com>
2016-09-30 07:53:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 var counter=0;
-element.onclick=function()
-{
-element.style.marginLeft="100px";
- var spam=document.getElementById('count');   
- counter=counter+1;
-  spam.innerHTML=counter.toString();
-};
+
 var button=document.getElementById('counter');
 button.onclick=function(){
  var spam=document.getElementById('count');   
