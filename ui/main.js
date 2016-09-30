cb5c43e1f44a7aa3551648a6d6e1dbe40f8730db
basavareddy25 <basavareddy25@gmail.com>
2016-09-30 11:59:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,28 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
-var counter=0;
+
 
  
 
 var c=document.getElementById('xyz');
 c.onclick= function()
 {
-   var spam=document.getElementById('count');   
- counter=counter+1;
-  var c=document.getElementById("xyz");
-  spam.innerHTML="i am bosss counter.toString()"; 
+ 
+  var request=new XMLHttpRequest();
+ 
+  request.onreadystatechange = function(){
+    // process the server response
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+        if(request.status==200)
+        {
+        var counter=request.responseText   ;
+         spam.innerHTML= counter.toString();
+  var request=new XMLHttpRequest();
+        }
+    }
+    };
+request.open('GET','http://basavareddy25.imad.hasura-app.io/counter',true);
 };
 
 
