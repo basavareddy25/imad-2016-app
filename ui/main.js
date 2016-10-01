fb01257cb0b193ad0b399d1fb6e20326651c0695
basavareddy25 <basavareddy25@gmail.com>
2016-10-01 20:39:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ var val=nip.value;
 var val=nip.value;
 
 
-    var names=['name1','manee','fhfjfkf'];
+    var names=request.responseText;
     var list="";
     for(var i=0;i<names.length;i++)
     {
