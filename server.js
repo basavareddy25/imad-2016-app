89252b0c89198cf43553a47a1ae6ae75aefb9423
basavareddy25 <basavareddy25@gmail.com>
2016-10-01 19:13:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/images.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'images.jpg'));
 });
+var names=[];
+app.get('/submit/:name', function (req, res) {
+  var name=req.perams.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
